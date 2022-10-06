96450006d037b9d9c781e52cdb26bf11d4de93e3
Kanch Ruansiripiyakul <digitazine@gmail.com>
2022-10-06 22:25:30 +0700

    ADD Fade background music

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Button : MonoBehaviour
 {
+    [SerializeField] private AudioSource _audioSource;
     public void StartGame(){
-         SceneManager.LoadScene(1);
+        _audioSource.DOFade(0.0f,1.0f);
+        Invoke("LoadScene",1f);
+    }
+
+    private void LoadScene(){
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame(){
